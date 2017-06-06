e2f13efa02ec69b147e4f23ffc931d86e3354fcd
Sarah Curry <scurry@student.ethz.ch>
2017-06-06 17:26:21 +0200

    added redistribution function

--- CodePE2/Estimator.m
+++ CodePE2/Estimator.m
@@ -79,7 +79,7 @@ Lx = 2*Ly ;
 %% Mode 1: Initialization
 % TO DO: how to tune the number of particles?
 % Set number of particles:
-N = 200; % obviously, you will need more particles than 10.
+N = 500; % obviously, you will need more particles than 10.
 if (init)
     % Do the initialization of your estimator here!
     % These particles are the posterior particles at discrete time k = 0
@@ -89,14 +89,14 @@ if (init)
     %     postParticles.y = zeros(2,N);
     %     postParticles.h = zeros(2,N);
     
-    % randomly distribute the initial estimates.
+    % randomly distribute the initial estimates between the 2 corners.
+    % robot A is always on the west side of the room; robot B is always on
+    % the left side.
     % postparticles is a 2xN matrix (2 robots x N samples).
-    
-    postParticles.x = [ones(2,N/4)*Lx ones(2,N/4)*Lx zeros(2,N/4) zeros(2,N/4)] ;
-    postParticles.y = [zeros(2,N/4) ones(2,N/4)*Ly ones(2,N/4)*Ly zeros(2,N/4)] ;
-    % make sure robots init headings point into the room
-    % TO DO: handle case where N/4 is not an integer.
-    %     postParticles.h = [ pi/2+rand([2,N/4])*pi/2 pi+rand([2,N/4])*pi/2 pi*3/2+rand([2,N/4])*pi/2 rand([2,N/4])*pi/2 ] ;
+    postParticles.x = [ones(1,N)*Lx ; zeros(1,N)] ;
+    postParticles.y = [zeros(2,ceil(N/2)) ones(2,floor(N/2))*Ly ] ;
+    % TO DO: make sure robots init headings point into the room
+    %     postParticles.h = [pi/2+rand([2,ceil(N/2)])*pi/2 pi+rand([2,N/4])*pi/2 pi*3/2+rand([2,N/4])*pi/2 rand([2,N/4])*pi/2 ] ;
     postParticles.h = rand(2,N)*2*pi ;
     
     % and leave the function
@@ -111,10 +111,9 @@ end % end init
 %% Step 1: Prior Update
 
 % Generate PDF of Process Noise and get N samples:
-vbar = KC.vbar;
-lower = -vbar ;
+lower = -KC.vsbar ;
 peak = 0 ;
-upper = vbar ;
+upper = KC.vsbar ;
 % PDF of process noise
 x_noise_pdf = makedist('Triangular','a',lower,'b',peak,'c',upper) ;
 % N values from pdf for robots A and B:
@@ -130,8 +129,8 @@ velocity.B = [act(2,1)*cos(prevPostParticles.h(2,:)) .* (1 + x_noise_val.B);
 % prior updates for x,y position coordinates of robots A and B.
 % KC.ts=timestep
 x_p_update.x(1,:) = prevPostParticles.x(1,:) + velocity.A(1,:)*KC.ts ;
-x_p_update.x(2,:) = prevPostParticles.x(2,:) + velocity.A(2,:)*KC.ts ;
-x_p_update.y(1,:) = prevPostParticles.y(1,:) + velocity.B(1,:)*KC.ts ;
+x_p_update.x(2,:) = prevPostParticles.x(2,:) + velocity.B(1,:)*KC.ts ;
+x_p_update.y(1,:) = prevPostParticles.y(1,:) + velocity.A(2,:)*KC.ts ;
 x_p_update.y(2,:) = prevPostParticles.y(2,:) + velocity.B(2,:)*KC.ts ;
 
 
@@ -157,21 +156,40 @@ hit.B.west = x_p_update.x(2,:) < 0 ;
 % same.
 x_p_update.h = prevPostParticles.h ;
 for p=1:N
-    if hit.A.north(1,p) || hit.A.south(1,p)
+    if hit.A.north(1,p)
         vj_error = heading_noise(prevPostParticles.h(1,p), 0) ;
         x_p_update.h(1,p) = (2*pi - prevPostParticles.h(1,p))*(1 + vj_error) ;
-    elseif hit.A.east(1,p) || hit.A.west(1,p)
+        x_p_update.y(1,p) = Ly - (x_p_update.y(1,p)-Ly) ; % Ly - error
+    elseif hit.A.south(1,p)
+        vj_error = heading_noise(prevPostParticles.h(1,p), 0) ;
+        x_p_update.h(1,p) = (2*pi - prevPostParticles.h(1,p))*(1 + vj_error) ;
+        x_p_update.y(1,p) = abs(x_p_update.y(1,p)) ;
+    elseif hit.A.east(1,p)
         vj_error = heading_noise(prevPostParticles.h(1,p), pi/2) ;
         x_p_update.h(1,p) = (pi - prevPostParticles.h(1,p))*(1 + vj_error) ;
-    elseif hit.B.north(1,p) || hit.B.south(1,p)
+        x_p_update.x(1,p) = Lx - (x_p_update.x(1,p)-Lx) ; % Lx - error
+    elseif hit.A.west(1,p)
+        vj_error = heading_noise(prevPostParticles.h(1,p), pi/2) ;
+        x_p_update.h(1,p) = (pi - prevPostParticles.h(1,p))*(1 + vj_error) ;
+        x_p_update.x(1,p) = abs(x_p_update.x(1,p)) ;
+    elseif hit.B.north(1,p)
         vj_error = heading_noise(prevPostParticles.h(2,p), 0) ;
         x_p_update.h(2,p) = (2*pi - prevPostParticles.h(2,p))*(1 + vj_error)  ;
-    elseif hit.B.east(1,p) || hit.B.west(1,p)
+        x_p_update.y(2,p) = Ly - (x_p_update.y(2,p)-Ly) ; % Ly - error
+    elseif hit.B.south(1,p)
+        vj_error = heading_noise(prevPostParticles.h(2,p), 0) ;
+        x_p_update.h(2,p) = (2*pi - prevPostParticles.h(2,p))*(1 + vj_error)  ;
+        x_p_update.y(2,p) = abs(x_p_update.y(2,p)) ;
+    elseif hit.B.east(1,p)
         vj_error = heading_noise(prevPostParticles.h(2,p), pi/2) ;
         x_p_update.h(2,p) = (pi - prevPostParticles.h(2,p))*(1 + vj_error) ;
+        x_p_update.x(2,p) = Lx - (x_p_update.x(2,p)-Lx) ; % Lx - error
+    elseif hit.B.west(1,p)
+        vj_error = heading_noise(prevPostParticles.h(2,p), pi/2) ;
+        x_p_update.h(2,p) = (pi - prevPostParticles.h(2,p))*(1 + vj_error) ;
+        x_p_update.x(2,p) = abs(x_p_update.x(2,p)) ;
     end
 end
-
 
 %% Generate PDF of Sensor Noise and get N samples:
 
@@ -183,6 +201,8 @@ upper = wbar ;
 z_noise_pdf = makedist('Triangular','a',lower,'b',peak,'c',upper) ;
 
 %% determine weights of particles
+% TO DO: make this a function. only execute if there are any sensor
+% measurements.
 
 % calculate distance from sensor to robot, based on x_p_update estimate.
 % distance is a 2xN matrix containing distances to each sensor
@@ -194,7 +214,6 @@ distance.s4 = sqrt(x_p_update.x.^2 + x_p_update.y.^2 ) ;
 % difference between z_meas and the distance (calculated from the prior) = z_noise.
 % determine noise for each sensor to each robot just in case we detect the
 % wrong robot.
-%         z_noise.A(i,:) = abs(sens(1,1) - distance.s1(1,:)) ;
 z_noise.s1.A = abs(sens(1,1) - distance.s1(1,:)) ; % s1 to A
 z_noise.s1.B = abs(sens(1,1) - distance.s1(2,:)) ; % s1 to B
 z_noise.s2.A = abs(sens(2,1) - distance.s2(1,:)) ; % s2 to A
@@ -205,6 +224,7 @@ z_noise.s4.A = abs(sens(4,1) - distance.s4(1,:)) ; % s4 to A
 z_noise.s4.B = abs(sens(4,1) - distance.s4(2,:)) ; % s4 to B
 
 % the probability of a sensor measurement with this noise is:
+% sbar0 = p(right robot) ; sbar1 = p(wrong robot)
 % robot A to s1 and s2
 f_z_given_xs.s1.sbar0 = pdf(z_noise_pdf, z_noise.s1.A ) ;
 f_z_given_xs.s1.sbar1 = pdf(z_noise_pdf, z_noise.s1.B ) ;
@@ -217,10 +237,10 @@ f_z_given_xs.s4.sbar0 = pdf(z_noise_pdf, z_noise.s4.B ) ;
 f_z_given_xs.s4.sbar1 = pdf(z_noise_pdf, z_noise.s4.A ) ;
 
 % apply Total Probability Theorem to find f_z_given_x for each sensor
-f_z_given_x.s1 = f_z_given_xs.s1.sbar0*KC.sbar + f_z_given_xs.s1.sbar1*(1-KC.sbar) ;
-f_z_given_x.s2 = f_z_given_xs.s2.sbar0*KC.sbar + f_z_given_xs.s2.sbar1*(1-KC.sbar) ;
-f_z_given_x.s3 = f_z_given_xs.s3.sbar0*KC.sbar + f_z_given_xs.s3.sbar1*(1-KC.sbar) ;
-f_z_given_x.s4 = f_z_given_xs.s4.sbar0*KC.sbar + f_z_given_xs.s4.sbar1*(1-KC.sbar) ;
+f_z_given_x.s1 = f_z_given_xs.s1.sbar0*(1-KC.sbar) + sum(f_z_given_xs.s1.sbar1)*KC.sbar ;
+f_z_given_x.s2 = f_z_given_xs.s2.sbar0*(1-KC.sbar) + sum(f_z_given_xs.s2.sbar1)*KC.sbar ;
+f_z_given_x.s3 = f_z_given_xs.s3.sbar0*(1-KC.sbar) + sum(f_z_given_xs.s3.sbar1)*KC.sbar ;
+f_z_given_x.s4 = f_z_given_xs.s4.sbar0*(1-KC.sbar) + sum(f_z_given_xs.s4.sbar1)*KC.sbar ;
 
 % Unnormalized likelihood
 f_x_prior.A = pdf(x_noise_pdf, x_noise_val.A) ; % probability of x_noise
@@ -240,7 +260,6 @@ end
 
 
 % likelihood of the state of robot B = f(s3)*f(s4)
-% TO DO: come back to this!
 if ~isinf(sens(3)) && ~isinf(sens(4))
     z_weights.B = (f_x_prior.B .* f_z_given_x.s3) .* (f_x_prior.B .* f_z_given_x.s4) ;
 elseif ~isinf(sens(3)) && isinf(sens(4))
@@ -252,17 +271,37 @@ elseif isinf(sens(3)) && isinf(sens(4))
 end
 
 % Normalized likelihood
-sum(z_weights.A) ;
-sum(z_weights.B) ;
 z_weights.A = z_weights.A / sum(z_weights.A) ;
 z_weights.B = z_weights.B / sum(z_weights.B) ;
+
+% if z_weights.A TO DO : handle case of lost particles.
+% if z_weights.A == 1/N
+%     redist_particles = redistribute_particles('robotA', sens, N, Lx, Ly) ;
+%     x_p_update.x(1,:) = redist_particles.x ;
+%     x_p_update.y(1,:) = redist_particles.y ;
+%     x_p_update.h(1,:) = redist_particles.h ;
+% end
+
+% if z_weights.B == 1/N
+%     redist_particles = redistribute_particles('robotB', sens, N, Lx, Ly) ;
+%     x_p_update.x(2,:) = redist_particles.x ;
+%     x_p_update.y(2,:) = redist_particles.y ;
+%     x_p_update.h(2,:) = redist_particles.h ;
+% end
 
 
 %% Resample: find the particle corresponding to the r bin
 
+% in the case where all particles are outside the sensor range and
+% therefore have zero (or equal) weight, we should resample from previous
+% particle estimates equally.
 postParticles.x = zeros(2,N);
 postParticles.y = zeros(2,N);
 postParticles.h = zeros(2,N);
+
+% if neither test passes for any particle, then redistribute the particles.
+tests_failed.A = 1;
+tests_failed.B = 1;
 
 for j=1:N
     % choose a random r between 0 and 1
@@ -280,48 +319,153 @@ for j=1:N
             postParticles.x(1,j) = x_p_update.x(1,p)  ;
             postParticles.y(1,j) = x_p_update.y(1,p)  ;
             postParticles.h(1,j) = x_p_update.h(1,p)  ;
-        end
+            tests_failed.A = 0 ;
+        end % end if
         
         if test1B && test2B
             postParticles.x(2,j) = x_p_update.x(2,p) ;
             postParticles.y(2,j) = x_p_update.y(2,p) ;
             postParticles.h(2,j) = x_p_update.h(2,p) ;
-        end
-        
-    end
-    
-    
-    %% Roughening
-    
-    
-    
-    %% Nested functions
-    
-    function vj_error = heading_noise(incident_angle, wall_angle)
-    % function determines the refelcted ideal angle and the error
-    % calculate heading noise.
-    c = 3/2*KC.vbar^3 ; % first find c by integrating over [-vbar, vbar]:
-    max_f_vj = c*KC.vbar^2 ; % calculate max f(vj) = c*vbar^2
-    f_vj = rand()*max_f_vj ; % choose a random f(vj)
-    vj_pos = sqrt(f_vj*c) ; % find the (positive only!) corresponding value of vj
-    vj_neg = -vj_pos ;
-    vj_pos_neg = [vj_pos vj_neg ] ;
-    
-    assign_pos_or_neg = randi(2) ;  % make vj randomly positive or negative
-    vj = vj_pos_neg(assign_pos_or_neg) ;
-    
-    % find angle alpha (which is the angle between the wall and the incident
-    % angle). heading error = alpha*vj
-    alpha = incident_angle ;
-    while (alpha - wall_angle) > pi/2
-        alpha = alpha - pi/2 ;
-    end
-    
-    vj_error = alpha*vj ;
-    
-    %     end % end function vj_error % TO DO : update matlab, uncomment this
-    %     end statement
-    
-    
+            tests_failed.B = 0 ;
+        end % end if
+    end % end p=1:N
+end % end for j=1:N
+
+% if tests fail for all particles, redistribute particles
+if tests_failed.A
+    redist_particles = redistribute_particles('robotA', sens, N, Lx, Ly) ;
+    postParticles.x(1,:) = redist_particles.x ;
+    postParticles.y(1,:) = redist_particles.y ;
+    postParticles.h(1,:) = redist_particles.h ;
+end
+if tests_failed.B
+    redist_particles = redistribute_particles('robotB', sens, N, Lx, Ly) ;
+    postParticles.x(2,:) = redist_particles.x ;
+    postParticles.y(2,:) = redist_particles.y ;
+    postParticles.h(2,:) = redist_particles.h ;
+end
+
+postParticles = roughen(postParticles, N) ; 
+
 end % end estimator
+
+
+%% Nested functions
+
+%% Roughening
+
+% define signma_i = K*E*N^(-1/d) = standard dev of the noise to add to the
+% particle.
+function roughened_particles = roughen(particles, N)
+K = 0.05 ; % K = tuning param << 1
+dimension = 3 ; % dimensions of state vector (x, y, h)
+Ei.A.x = max(particles.x(1,:)) - min(particles.x(1,:)) ;
+Ei.A.y = max(particles.y(1,:)) - min(particles.y(1,:)) ;
+Ei.A.h = max(particles.h(1,:)) - min(particles.h(1,:)) ;
+Ei_total.A = sqrt(Ei.A.x^2 + Ei.A.y^2 + Ei.A.h^2) ;
+sigma_i.A = K*Ei_total.A*N^(-1/dimension) ;
+roughening_pdf.A = makedist('Normal', 'sigma' , sigma_i.A ) ;
+roughening_noise.A = random(roughening_pdf.A, 1, N) ;
+roughened_particles.x(1,:) = particles.x(1,:) + roughening_noise.A ;
+roughened_particles.y(1,:) = particles.y(1,:) + roughening_noise.A ;
+roughened_particles.h(1,:) = particles.h(1,:) + roughening_noise.A ;
+
+if size(particles.x,1)==2
+    Ei.B.x = max(particles.x(2,:)) - min(particles.x(2,:)) ;
+    Ei.B.y = max(particles.y(2,:)) - min(particles.y(2,:)) ;
+    Ei.B.h = max(particles.h(2,:)) - min(particles.h(2,:)) ;
+    Ei_total.B = sqrt(Ei.B.x^2 + Ei.B.y^2 + Ei.B.h^2) ;
+    sigma_i.B = K*Ei_total.B*N^(-1/dimension) ;
+    roughening_pdf.B = makedist('Normal', 'sigma' , sigma_i.B ) ;
+    roughening_noise.B = random(roughening_pdf.B, 1, N) ;
+    roughened_particles.x(2,:) = particles.x(2,:) + roughening_noise.B ;
+    roughened_particles.y(2,:) = particles.y(2,:) + roughening_noise.B ;
+    roughened_particles.h(2,:) = particles.h(2,:) + roughening_noise.B ;
+end % end if
+end % end function roughen
+
+function vj_error = heading_noise(incident_angle, wall_angle)
+% function determines the refelcted ideal angle and the error
+% calculate heading noise.
+c = 3/2*KC.vbar^3 ; % first find c by integrating over [-vbar, vbar]:
+max_f_vj = c*KC.vbar^2 ; % calculate max f(vj) = c*vbar^2
+f_vj = rand()*max_f_vj ; % choose a random f(vj)
+vj_pos = sqrt(f_vj*c) ; % find the (positive only!) corresponding value of vj
+vj_neg = -vj_pos ;
+vj_pos_neg = [vj_pos vj_neg ] ;
+
+assign_pos_or_neg = randi(2) ;  % make vj randomly positive or negative
+vj = vj_pos_neg(assign_pos_or_neg) ;
+
+% find angle alpha (which is the angle between the wall and the incident
+% angle). heading error = alpha*vj
+alpha = incident_angle ;
+while (alpha - wall_angle) > pi/2
+    alpha = alpha - pi/2 ;
+end
+
+vj_error = alpha*vj ;
+
+end % end vj_error
+
+
+function new_particles = redistribute_particles(robot, sens, N, Lx, Ly)
+% if all particles have equal weights, then we have lost track of the
+% robot and should randomly redistribute them.
+
+if robot == 'robotA'
+    if ~isinf(sens(1)) && ~isinf(sens(2))
+        % redistribute sbar% of particles for robot A;
+        new_particles.h = rand(1,N)*2*pi ;
+        theta_s1_s2 =  [rand(1,floor(N/2))*pi/2 + pi/2 rand(1,floor(N/2))*pi/2 + pi ] ;
+        new_particles.x = [Lx+sens(1)*cos(theta_s1_s2(1:floor(N/2))) Lx+sens(2)*cos(theta_s1_s2(floor(N/2+1):end)) ]; % cos(theta) < 0, so add this to Lx
+        new_particles.y = [sens(1)*sin(theta_s1_s2(1:floor(N/2))) Ly+sens(2)*sin(theta_s1_s2(floor(N/2)+1:end))] ;
+    elseif ~isinf(sens(1))
+        new_particles.h = rand(1,N)*2*pi ;
+        theta_s1 = rand(1,N)*pi/2 + pi/2 ;
+        new_particles.x = Lx + sens(1)*cos(theta_s1) ; % cos(theta) < 0, so add this to Lx
+        new_particles.y = sens(1)*sin(theta_s1) ;
+    elseif ~isinf(sens(2))
+        new_particles.h = rand(1,N)*2*pi ;
+        theta_s2 = rand(1,N)*pi/2 + pi ;
+        new_particles.x = Lx + sens(2)*cos(theta_s2) ;
+        new_particles.y = Ly+sens(2)*sin(theta_s2) ;
+    else
+        new_particles.h = rand(1,N)*2*pi ;
+        new_particles.x = rand(1,N)*Lx;
+        new_particles.y = rand(1,N)*Ly ;
+    end
+end
+
+if robot == 'robotB'
+        new_particles.h = rand(1,N)*2*pi ;
+        new_particles.x = rand(1,N)*Lx;
+        new_particles.y = rand(1,N)*Ly ;
     
+%         if ~isinf(sens(3)) && ~isinf(sens(4))
+%             redistribute sbar% of particles for robot A;
+%             new_particles.h = rand(1,N)*2*pi ;
+%             theta_s1_s2 =  [rand(1,floor(N/2))*pi/2 + pi/2 rand(1,floor(N/2))*pi/2 + pi ] ;
+%             new_particles.x = [Lx+sens(1)*cos(theta_s1_s2(1:floor(N/2))) Lx+sens(2)*cos(theta_s1_s2(floor(N/2+1):end)) ]; % cos(theta) < 0, so add this to Lx
+%             new_particles.y = [sens(1)*sin(theta_s1_s2(1:floor(N/2))) Ly+sens(2)*sin(theta_s1_s2(floor(N/2)+1:end))] ;
+%         elseif ~isinf(sens(1))
+%             new_particles.h = rand(1,N)*2*pi ;
+%             theta_s1 = rand(1,N)*pi/2 + pi/2 ;
+%             new_particles.x = Lx + sens(1)*cos(theta_s1) ; % cos(theta) < 0, so add this to Lx
+%             new_particles.y = sens(1)*sin(theta_s1) ;
+%         elseif ~isinf(sens(2))
+%             new_particles.h = rand(1,N)*2*pi ;
+%             theta_s2 = rand(1,N)*pi/2 + pi ;
+%             new_particles.x = Lx + sens(2)*cos(theta_s2) ;
+%             new_particles.y = Ly+sens(2)*sin(theta_s2) ;
+%         else
+%             new_particles.h = rand(1,N)*2*pi ;
+%             new_particles.x = rand(1,N)*Lx;
+%             new_particles.y = rand(1,N)*Ly ;
+%         end
+end
+
+end
+
+
+
